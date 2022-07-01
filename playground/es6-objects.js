106997cebc3115d5c3068bcb1548a2aff8491bf4
briankmbugua <briankmbuguak@gmail.com>
2022-07-02 01:11:43 +0300

    object destructuring

---
+++ playground/es6-objects.js
@@ -0,0 +1,36 @@
+//object property shorthand
+
+const name = 'Brian'
+const userAge = 99
+
+const user = {
+    name, //Object property shorthand
+    age: userAge,
+    location: 'mars'
+}
+
+console.log(user);
+
+//object destructuring
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 202,
+    salesPrice: undefined
+}
+
+// const label = product.label
+// const {label:productlabel, stock, rating = 5} = product
+
+// // console.log(label);
+// console.log(stock);
+// console.log(productlabel);
+// console.log(rating);
+
+
+const transaction =  (type,{label, stock}) => {
+    console.log(type,label,stock);
+}
+
+transaction('order',product)
