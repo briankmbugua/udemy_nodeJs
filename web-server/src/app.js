90488c9ce27f99488a72d969af5ecd29eca070d6
briankmbugua <briankmbuguak@gmail.com>
2022-07-06 22:07:37 +0300

    handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,32 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname);
-console.log(path.join(__dirname, '../public'));
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+app.get('',(req,res)=>{
+    res.render('index', {
+        title: 'Weather',
+        name: 'Brian Kinyanjui'
+    })
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about',{
+        title: 'About me',
+        name: 'Brian Kinyanjui'
+    })
+})
+
+app.get('/help', (req,res)=>{
+    res.render('help',{
+        title: 'help page',
+        name: 'Brian Kinyanjui'
+    })
+})
 
 app.get('/weather',(req,res)=>{
     res.send({
