768760588dd13c8149d3d2797c7d09c7623fedbb
briankmbugua <briankmbuguak@gmail.com>
2022-07-14 23:13:40 +0300

    mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,24 +1,79 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 const User = mongoose.model('User',{
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        dafault: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if(value.includes('password')) {
+                throw new Error('Password must not contain the string "password" ')
+            }
+        }
     }
 })
 
-const me = new User({
-    name: 'Brian',
-    age: 50
+// const me = new User({
+//     name: '   Mbugua   ',
+//     email: 'BRIANKINYANJUI@GMAIL.COM',
+//     password: 'password123'
+// })
+
+// me.save().then(()=>{
+//     console.log(me)
+// }).catch((error)=>{
+//     console.log('Error', error);
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
-me.save().then(()=>{
-    console.log(me)
+
+const task = new Task({
+    description: 'finish backend development'
+})
+
+task.save().then(()=>{
+    console.log(task)
 }).catch((error)=>{
-    console.log('Error', error);
+    console.log('Error!', error);
 })
