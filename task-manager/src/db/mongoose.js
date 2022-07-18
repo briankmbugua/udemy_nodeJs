bcb7de5724e8f70d1ecfd416053de8cd9b7c8c2b
briankmbugua <briankmbuguak@gmail.com>
2022-07-19 00:04:24 +0300

    mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,14 +4,14 @@ const validator = require('validator')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String,
+//         trim: true,
+//         required: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     }
+// })
