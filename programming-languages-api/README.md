f859f3c20ce83f1a4969a470d124436a5bda9142
briankmbugua <briankmbuguak@gmail.com>
2023-11-26 19:25:52 +0300

    programming-languages-api

---
+++ programming-languages-api/README.md
@@ -0,0 +1,4 @@
+GET /programming-languages → getMultiple()
+POST /programming-languages → create()
+PUT /programming-languages/:id → update()
+DELETE /programming-languages/:id → remove()
---
+++ programming-languages-api/config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "programming_api",
+        connectTimeout: 6000
+    },
+    listPerPage: 10,
+};
+
+module.exports = config;
